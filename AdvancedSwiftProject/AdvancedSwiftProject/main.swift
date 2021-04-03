07a2abf3257646f306f95a627c29ea7b38ec73e1
Sherry1481 <shareyscorpi1@gmail.com>
2021-04-03 07:39:12 +0500

    commit

---
+++ AdvancedSwiftProject/AdvancedSwiftProject/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  AdvancedSwiftProject
+//
+//  Created by sherry on 03/04/2021.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ AngryBirdClone/AngryBirdClone/GameScene.swift
@@ -0,0 +1,57 @@
+//
+//  GameScene.swift
+//  AngryBirdClone
+//
+//  Created by sherry on 02/04/2021.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var bird2 = SKSpriteNode()
+    
+    
+    
+    override func didMove(to view: SKView) {
+        
+       
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        }
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
