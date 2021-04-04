b4eb4a988a7b50b6da4b603211790784ad5eaab4
Sherry1481 <shareyscorpi1@gmail.com>
2021-04-05 00:14:39 +0500

    c

---
+++ SnapchatClone/SnapchatClone/Model/Snap.swift
@@ -0,0 +1,16 @@
+//
+//  Snap.swift
+//  SnapchatClone
+//
+//  Created by sherry on 04/04/2021.
+//
+
+
+import Foundation
+
+struct Snap {
+    var username: String
+    var imageUrlArray : [String]
+    var date : Date
+    var timeDifference : Int
+}
