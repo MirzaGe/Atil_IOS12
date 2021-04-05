0b5277619e9af5f799a6380b6f5ce1db692e500a
Sherry1481 <shareyscorpi1@gmail.com>
2021-04-05 13:25:32 +0500

    commit

---
+++ MachineLearningImageRecognition/MachineLearningImageRecognition/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MachineLearningImageRecognition
+//
+//  Created by sherry on 05/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func changeClicked(_ sender: Any) {
+    }
+    
+}
+
